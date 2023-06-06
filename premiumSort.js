00edbbd1912756f45549f0d9865bd0866d09ee63
Sablic Ivana <ivasabl@pr5-11.studenti.math.hr>
2023-06-06 16:35:11 +0200

    omogucen sort u sucelju za premium usere i uskladen css s ostatkom stranice

---
+++ premiumSort.js
@@ -0,0 +1,168 @@
+$(document).ready(function() {
+  var choice = $('.pdecorated-label');
+  console.log(choice.length);
+
+  for (var i = 0; i < choice.length; i++) {
+    (function(index) { // Create a closure to capture the current value of `i`
+      $('.pdecorated-label').eq(index).on('click', function() {
+        console.log(choice.eq(index).css('background-color'));
+
+        if (choice.eq(index).css('background-color') === "rgb(221, 221, 221)"
+        || choice.eq(index).css('background-color') === "rgb(242, 242, 242)") {
+          choice.eq(index).css('background-color', "rgb(176, 172, 172)");
+      		for(var j = 0; j < choice.length; j++){
+        		if(j !== index){
+              choice.eq(j).css('background-color', "rgb(242, 242, 242)");
+        		}
+        	}
+        }else {
+          choice.eq(index).css('background-color', "rgb(242, 242, 242)");
+        }
+      });
+    })(i); // Pass the current value of `i` to the closure
+  }
+
+$('#pSort').on('click', function(){
+	var choice = $('.pdecorated-label');
+	for (var i = 0; i < choice.length; i++){
+		if(choice.eq(i).css('background-color') === "rgb(176, 172, 172)"){
+			console.log("hi, id = " + choice.eq(i).attr("id"));
+      if(choice.eq(i).attr("id") === "dl1"){//silazni sort po cijeni
+				sortPPrice(1);
+			}else if(choice.eq(i).attr("id") === "dl2"){
+				sortPPrice(2);
+			}
+			if(choice.eq(i).attr("id") === "dl3"){//silazni sort po tipu
+				sortType(3);
+			}else if(choice.eq(i).attr("id") === "dl4"){
+				sortType(4);
+			}
+		}
+	}
+  });
+
+function sortType(k) {
+	//ako k==3 silazno, inace uzlazno
+      //sad znam koji sort moram napraviti, ali da bi sortirala prvo moram dohvatiti listu
+      var table = $("#premiumSort td");
+      var oldList = $('#premiumSort tr');
+      var len = $('#premiumSort tr');
+      var n = len.length; //broj redaka
+      var j = 0;
+      function Room(id, tip, price, fname) {
+        this.tip =  tip.substring(10);
+        this.price = Number(price.substring(13));
+        this.id = id.substring(9);
+        this.fname = id.substring(9);
+      }
+
+      function usporeditip2(a, b){
+        tip1 = a.tip.toLowerCase();
+        tip2 = b.tip.toLowerCase();
+        return tip1 > tip2;
+      }
+
+      function usporeditip1(a, b){
+        tip1 = a.tip.toLowerCase();
+        tip2 = b.tip.toLowerCase();
+        return tip1 < tip2;
+      }
+
+      Room.prototype = {
+        constructor: Room, // treba jer {} kreira NOVI objekt
+      };
+      let arrRooms = new Array();
+      var j = 0;
+      for(var i = 0; i < n; i++){
+        console.log("ime = " + $("#premiumSort td").eq(4*i + 1).html());
+        arrRooms[i] = new Room($("#premiumSort td").eq(4*i).html(), $("#premiumSort td").eq(4*i + 1).html(), $("#premiumSort td").eq(4*i + 2).html(), $("#premiumSort td").eq(4*i).html());
+      }
+
+      if(k === 3){
+        arrRooms.sort(usporeditip2);
+      }else {
+        arrRooms.sort(usporeditip1);
+      }
+
+      oldList.remove();
+      for(var i = 0; i < n; i++){
+        $('#premiumSort').append(
+              '<tr>' +
+                '<td class="room">id sobe: ' + arrRooms[i].id + '</td>' +
+                '<td class="room">tip sobe: ' + arrRooms[i].tip + '</td>' +
+                '<td class="room">cijena sobe: ' + arrRooms[i].price + '</td>' +
+                '<td class="room">' +
+                  '<form class="" action="index.php?rt=hotels/removeroom" method="post">' +
+                    '<input type="submit" name="' + arrRooms[i].fname + '" value="X">' +
+                  '</form>' +
+                '</td>' +
+              '</tr>');
+
+      }
+  }
+
+  function sortPPrice(k) {
+    console.log("sor špo cijeni");
+  	//ako k==1 silazno, inace uzlazno
+        //sad znam koji sort moram napraviti, ali da bi sortirala prvo moram dohvatiti listu
+        var table = $("#premiumSort td");//tu su ćelije tablice sa sobama
+        var oldList = $('#premiumSort tr');
+        var len = $('#premiumSort tr');
+        var n = len.length; //broj redaka
+        var j = 0;
+        function Room(id, tip, price, fname) {
+          this.tip = tip.substring(10);
+          this.price = Number(price.substring(13));
+          this.id = id.substring(9);
+          this.fname = id.substring(9);
+        }
+
+        function usporediCijenu1(a, b){
+          return a.price > b.price;
+        }
+
+        function usporediCijenu2(a, b){
+          return a.price < b.price;
+        }
+
+        Room.prototype = {
+          constructor: Room, // treba jer {} kreira NOVI objekt
+        };
+        let arrRooms = new Array();
+        let arrPrice = new Array();
+        var j = 0;
+        for(var i = 0; i < n; i++){
+          arrRooms[i] = new Room($("#premiumSort td").eq(4*i).html(), $("#premiumSort td").eq(4*i + 1).html(), $("#premiumSort td").eq(4*i + 2).html(), $("#premiumSort td").eq(4*i).html());
+          console.log("i = " + 4*i);
+          console.log("tip = " + arrRooms[i].tip);
+          console.log("i = " + (4*i+1));
+          console.log("id = " + arrRooms[i].id);
+          console.log("i = " + (4*i+2));
+          console.log("price = " + arrRooms[i].price);
+          console.log("i = " + (4*i));
+          console.log("fname = " + arrRooms[i].fname);
+
+        }
+
+        if(k === 1){
+          arrRooms.sort(usporediCijenu2);
+        }else {
+          arrRooms.sort(usporediCijenu1);
+        }
+
+        oldList.remove();
+        for(var i = 0; i < n; i++){
+          $('#premiumSort').append(
+                '<tr>' +
+                  '<td class="room">id sobe: ' + arrRooms[i].id + '</td>' +
+                  '<td class="room">tip sobe: ' + arrRooms[i].tip + '</td>' +
+                  '<td class="room">cijena sobe: ' + arrRooms[i].price + '</td>' +
+                  '<td class="room">' +
+                    '<form class="" action="index.php?rt=hotels/removeroom" method="post">' +
+                      '<input type="submit" name="' + arrRooms[i].fname + '" value="X">' +
+                    '</form>' +
+                  '</td>' +
+                '</tr>');
+        }
+    }
+});
--- view/premium_header.php
+++ view/premium_header.php
@@ -4,15 +4,17 @@
 	<meta charset="utf8">
 	<title>IDC Booking</title>
 	<link rel="stylesheet" href="<?php echo __SITE_URL;?>/css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.js"></script>
 	<script src="https://kit.fontawesome.com/512d1b1458.js" crossorigin="anonymous"></script>
-</head>
+	<script src="bookingScript.js"></script>
+	<script src="premiumSort.js"></script>
 <body>
 	<div class="navbar">
 		<h1 class="mainHeader">IDC Booking</h1>
 		<ul>
 			<li><a class="a" href="<?php echo __SITE_URL; ?>/index.php?rt=hotels/availableHotels"><i class="fas fa-house"></i> Available hotels</a></li>
-			<li><a href="<?php echo __SITE_URL; ?>/index.php?rt=hotels/premiumindex">Your offers</a></li>
-			<li id="logout"><a href="<?php echo __SITE_URL; ?>/index.php?rt=hotels">Logout</a></li>
+			<li><a href="<?php echo __SITE_URL; ?>/index.php?rt=hotels/premiumindex"><i class="fas fa-book"></i>Your offers</a></li>
+			<li id="logout"><a href="<?php echo __SITE_URL; ?>/index.php?rt=hotels"><i class="fas fa-lock"></i>Logout</a></li>
 			<li><i class="fas fa-user"> </i><?php
 					echo " " . htmlentities($_SESSION["username"], ENT_QUOTES);?></li>
 		</ul>
